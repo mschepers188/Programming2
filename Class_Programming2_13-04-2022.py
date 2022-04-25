9dc481758435671a72608dbe0d4546f92b5b6c8d
Martin Aimé Schepers <81292024+mschepers188@users.noreply.github.com>
2022-04-25 10:30:57 +0200

    Checkpoint

---
+++ Class_Programming2_13-04-2022.py
@@ -0,0 +1,8 @@
+# Class Programming2 13-04-2022
+
+# Class diagram & sequence diagram
+# You can make containers that contain the different classes
+# Sequence diagram -> Producers and consumers
+# plantuml.com for charts
+# refactoring guru
+# Decorator Patterns
